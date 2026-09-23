96dbc3421d7db66a89542bd77b91fb2ff960f119
ChristineG77 <gilbertc0069@student.faytechcc.edu>
2026-09-23 14:35:48 +0000

    FInal Final

--- Classwork/M3CW1_Gilbert.java
+++ Classwork/M3CW1_Gilbert.java
@@ -4,13 +4,12 @@ public class M3CW1_Gilbert
         {
             System.out.println("Christine");
             System.out.println("Gilbert");
-            System.out.println("");
-            System.out.println("Fall 2026 Class Schedule:");
-            System.out.println("");
+            System.out.println("Fall 2026 Class Schedule");
             System.out.println("CCIS-115-0001 Introduction to Programming and Logic");
             System.out.println("CTI-110-0003 IT Foundations");
             System.out.println("CTI-120-0002 Network & Security Foundations");
             System.out.println("PHI-240-0902 Introduction to Ethics");
+            System.out.println("I have around 1-2 years of basic coding experience in C#, and under a year of experience in Java and Python");
 
         }    
 
