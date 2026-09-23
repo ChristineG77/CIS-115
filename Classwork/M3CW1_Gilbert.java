f6a8f169cd0f27944d73964cfd68d4ef86e30694
ChristineG77 <gilbertc0069@student.faytechcc.edu>
2026-09-23 14:31:50 +0000

    Final

---
+++ Classwork/M3CW1_Gilbert.java
@@ -0,0 +1,18 @@
+public class M3CW1_Gilbert 
+{
+        public static void main(String[] args)
+        {
+            System.out.println("Christine");
+            System.out.println("Gilbert");
+            System.out.println("");
+            System.out.println("Fall 2026 Class Schedule:");
+            System.out.println("");
+            System.out.println("CCIS-115-0001 Introduction to Programming and Logic");
+            System.out.println("CTI-110-0003 IT Foundations");
+            System.out.println("CTI-120-0002 Network & Security Foundations");
+            System.out.println("PHI-240-0902 Introduction to Ethics");
+
+        }    
+
+
+}
--- Classwork/Practicing.java
+++ Classwork/Practicing.java
@@ -1,19 +1,21 @@
 /** 
- * M3CW1 Assignment
- * 115
- * Christine GIlbert
+ * Practicing
+ * publc class must be the same name as the file name
  */
 
-public class M3CW1_Gilbert
+public class Practicing
 {
+
+    public static void main(String[] args)
+    {
     System.out.println("Christine");
     System.out.println("Gilbert");
-    System.out.println("2025 Fall Schedule");
+    System.out.println("2026 Fall Schedule");
     System.out.println("CIS-115-0001 Intro to Porgramming and Logic");
     System.out.println("CTI-110-0003 IT Foundations");
     System.out.println("CTI-120-0002 Network & Security Foundations");
     System.out.println("PHI-240-0902 Introduction to Ethics");
-
+    }
 
 
 }
