60c29eaa9c0500dfe6cdede4b3947b21073586f8
ChristineG77 <gilbertc0069@student.faytechcc.edu>
2026-09-23 14:55:50 +0000

    Start and Finish

---
+++ Classwork/M3CW2_Gilbert.java
@@ -0,0 +1,54 @@
+public class M3CW2_Gilbert 
+{
+
+    public static void main(String[] args)
+    {
+        //Integer types
+        //byte: very small whole number
+
+        byte myByte = 100;
+
+        //short: larger than a byte
+        short myShort = 20000;
+
+        //int: most common integer type
+        int myInt = 100000;
+
+
+        //long: very large whole numbers that have L at the end
+        long myLong = 10000000000L;
+
+        //Decimal and float point types
+
+        //float:numbers with decimals and low precision with an f at the end
+        float myFLoat = 3.14f;
+
+        //double: numbers with high precision and is the default type
+        double myDouble = 3.14159265359;
+
+        //character and booleans
+
+        //char:single character in single quotes
+        char myChar = 'A';
+
+        //boolean is a true or false
+        boolean myBoolean = true;
+
+        //reference type
+        //string: squenece if characters (words) in double quotes
+        String myString = "Hello java";
+
+        //Display values
+        System.out.println("Byte Value:" + myByte);
+        System.out.println("Short Value:" + myShort);
+        System.out.println("Integer Value:" + myInt);
+        System.out.println("Long Value:" + myLong);
+        System.out.println("Float Value:" + myFLoat);
+        System.out.println("Double Value:" + myDouble);
+        System.out.println("Character Value:" + myChar);
+        System.out.println("Boolean Value:" + myBoolean);
+        System.out.println("String Value:" + myString);
+
+    }
+    
+}
