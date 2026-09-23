f00e6ba8236b002a6a2a1ebd500a6e6ed8b170d4
ChristineG77 <gilbertc0069@student.faytechcc.edu>
2026-09-23 14:13:02 +0000

    changes

---
+++ Classwork/Practicing.java
@@ -0,0 +1,19 @@
+/** 
+ * M3CW1 Assignment
+ * 115
+ * Christine GIlbert
+ */
+
+public class M3CW1_Gilbert
+{
+    System.out.println("Christine");
+    System.out.println("Gilbert");
+    System.out.println("2025 Fall Schedule");
+    System.out.println("CIS-115-0001 Intro to Porgramming and Logic");
+    System.out.println("CTI-110-0003 IT Foundations");
+    System.out.println("CTI-120-0002 Network & Security Foundations");
+    System.out.println("PHI-240-0902 Introduction to Ethics");
+
+
+
+}
